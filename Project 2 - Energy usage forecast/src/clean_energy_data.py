cec30269e0b6bb3b087987f70f26b570f77aaaea
Voortuinkabouter <dlvanuunen@live.nl>
2026-08-27 15:07:15 +0200

    added weather data and further data cleaning, preparing for merge

--- Project 2 - Energy usage forecast/src/clean_energy_data.py
+++ Project 2 - Energy usage forecast/src/clean_energy_data.py
@@ -1,75 +1,82 @@
-
-
 import pandas as pd
 from pathlib import Path
 import plotly.graph_objects as go
 import plotly.io as pio
 pio.renderers.default = "browser"
 
-
+# Load combined dataset
 data_file = Path("./Project 2 - Energy usage forecast/data/combined_hourly_load2.csv")
 print(data_file)
 
-#Explore dataset
 df = pd.read_csv(data_file, parse_dates=['DateUTC'])
-print(df.dtypes)
-print(df.head(5))
-print(df.describe())
-value_mean = df["Value"].mean()
-print(value_mean)
-
-#set index to timestamp and sort
-df = df.set_index('DateUTC').sort_index()
 
 
 #Filter for Netherlands values only
 df_NL= df[df['CountryCode']=='NL']
 
-
-#prepare plot
-x = df_NL.index
-y = df_NL['Value']
-
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter(
-        x=x,
-        y=y,
-        mode='lines'
-    )
-)
-fig.show()
-
-
-#resampling for daily median values of the load
-df_NL_daily = df_NL['Value'].resample('D').mean()
-x = df_NL_daily.index
-y = df_NL_daily
-
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter(
-        x=x,
-        y=y,
-        mode='lines'
-    )
-)
-fig.show()
+#set index to timestamp and sort
+df_NL = df_NL.sort_values("DateUTC")
+df_NL = df_NL.set_index('DateUTC')
 
 
 
-#resampling for monthly median values of the load
-df_NL_monthly = df_NL['Value'].resample('ME').median()
-x = df_NL_monthly.index
-y = df_NL_monthly
 
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter(
-        x=x,
-        y=y,
-        mode='lines'
-    )
-)
 
-fig.show()
+
+
+#Check / explore dataset
+print("head:", df_NL.head(5))
+print("tail:", df.tail(5))
+# print(df_NL.dtypes)
+print(df_NL.describe())
+# value_mean = df_NL["Value"].mean()
+# print(value_mean)
+
+
+# #prepare plot
+# x = df_NL.index
+# y = df_NL['Value']
+
+# fig = go.Figure()
+# fig.add_trace(
+#     go.Scatter(
+#         x=x,
+#         y=y,
+#         mode='lines'
+#     )
+# )
+# fig.show()
+
+
+# #resampling for daily median values of the load
+# df_NL_daily = df_NL['Value'].resample('D').mean()
+# x = df_NL_daily.index
+# y = df_NL_daily
+
+# fig = go.Figure()
+# fig.add_trace(
+#     go.Scatter(
+#         x=x,
+#         y=y,
+#         mode='lines'
+#     )
+# )
+# fig.show()
+
+
+
+# #resampling for monthly median values of the load
+# df_NL_monthly = df_NL['Value'].resample('ME').median()
+# x = df_NL_monthly.index
+# y = df_NL_monthly
+
+# fig = go.Figure()
+# fig.add_trace(
+#     go.Scatter(
+#         x=x,
+#         y=y,
+#         mode='lines'
+#     )
+# )
+
+# fig.show()
---
+++ Project 2 - Energy usage forecast/src/clean_weather_data.py
@@ -0,0 +1,41 @@
+# Data source: https://www.daggegevens.knmi.nl/klimatologie/uurgegevens
+import pandas as pd
+from pathlib import Path
+
+# Load datafile
+data_file = Path("./Project 2 - Energy usage forecast/data/weather.csv")
+df= pd.read_csv(data_file, skiprows=17)
+
+# Clean column names, renamed to more explicit feature names
+df.columns = df.columns.str.strip()
+df = df.rename(columns={
+    "# STN": "Station number",
+"YYYYMMDD": "Date",
+"HH": "Time",
+"T": "Temperature",
+"U": "Relative humidity",
+"FH": "Average wind speed",
+"FX": "Maximum wind gust",
+"SQ": "Sunshine duration",
+"Q": "Global radiation",
+"N": "Cloud cover",
+"RH": "Precipitation amount",
+"P": "Air pressure",
+   })
+
+# Add DateTime column
+df["DateTime"] = (pd.to_datetime(df["Date"], format="%Y%m%d") + pd.to_timedelta(df["Time"], unit="h"))
+
+
+df = df.sort_values("DateTime")
+df = df.set_index("DateTime")
+
+# check
+print(df.head(5))
+print(df.tail(5))
+print(df.describe())
+
+
+
+
+
